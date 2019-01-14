40fea339f53f36f7958c9485155a7968a4e3ce95
Brandon <41562804+bthecobb@users.noreply.github.com>
2019-01-13 21:55:51 -0500

    Files to use for basic .net core web api

---
+++ Todo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace webapitest.Models
+{
+    public class Todo :DbContext
+    {
+        public Todo(DbContextOptions<Todo> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ TodoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webapitest.Models;
+using System;
+
+namespace webapitest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly Todo _context;
+        public TodoController(Todo context)
+        {
+            _context = context;
+
+            if(_context.TodoItems.Count() ==0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoitem = await _context.TodoItems.FindAsync(id);
+
+            if(todoitem == null)
+            {
+                return NotFound();
+            }
+            return todoitem;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        }
+
+
+    }
+}
